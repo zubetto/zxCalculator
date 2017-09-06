bba2626e58ea4b821aeaf864d436274be8edec2c
Zubetto85 <Zubetto85@ZUBETTO85-PC>
2017-09-07 00:14:24 +0400

    ColorControl sliders click fixed

--- zxCalculator/Properties/AssemblyInfo.cs
+++ zxCalculator/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Windows;
 [assembly: AssemblyTitle("zxCalculator")]
 [assembly: AssemblyDescription("Stable. Output via filter: PointsSampler")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AlexanderZ")]
 [assembly: AssemblyProduct("zxCalculator")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  Alexander Zubov 2017")]
+[assembly: AssemblyTrademark("Zubetto")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.2")]
+[assembly: AssemblyFileVersion("1.0.1.2")]
